868eae31a534283db072e453c4424b9174403717
Alexey Novichikhin <novichikhin.au@gmail.com>
2021-06-17 18:46:00 +0300

    add action

---
+++ README.md
@@ -0,0 +1,2 @@
+#  kube-create-service-deployment
+kubectl create 'deployment.yaml' and 'servie.yaml' from specific folder
---
+++ entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# exit when any command fails
+set -e
+
+# keep track of the last executed command
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+# echo an error message before exiting
+trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
+
+# Extract the base64 encoded config data and write this to the KUBECONFIG
+echo "$KUBE_CONFIG_DATA" | base64 --decode > /tmp/config
+export KUBECONFIG=/tmp/config
+
+kubectl config current-context
+kubectl get nodes
+ls -la
+
+cd $FOLDER
+kubectl create service.yaml
+kubectl create deployment.yaml
+
+
+rm /tmp/config
